9d60dab22bddd675e54b50a633912e5bfdc58c59
James Demiraiakian <james.armen@gmail.com>
2022-02-11 15:19:28 -0800

    dark mode styles done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
     <div className='header'>
       <div>{user ? <div>Welcome to the guestbook, {user}!</div> : <div>Welcome Guest!</div>}</div>
       <button
-        className={darkMode.value ? 'button-dark-mode' : 'button-light-mode'}
+        className={darkMode.value ? 'style-button-dark-mode' : 'style-button-light-mode'}
         type="button"
         onClick={handleDark}
       >
